7ec01518a5377ef2ff2e7f1f0192c201a1aa9e52
MohammedAias <87969098+MohammedAias@users.noreply.github.com>
2023-06-01 11:09:40 +0400

    Initial Commit

---
+++ GMGorders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GMGorders
+//
+//  Created by Mohammed Ajas on 6/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
